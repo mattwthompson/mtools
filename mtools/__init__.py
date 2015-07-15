22c42bb286e6ce42b3befe55eaf4ee1e6959ba62
mattwthompson <mattwthomp@gmail.com>
2015-07-15 07:43:28 -0700

    package formalities initialized

---
+++ mtools/__init__.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def read_reaxc_bonds(bonds_file = 'bonds.reaxc', cutoff=0.3):
+    #takes bonds file, minimin 'bond order' to consider
+    #returns Nx3, columns atom_1, atom_2, bond_order
+    bonds = np.ndarray(shape=(0,3))
+    
+    with open(bonds_file,'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            else:
+                fields = line.split()
+                nb = int(fields[2])
+                for b in range(nb):
+                    if float(fields[4+nb+b]) > cutoff:
+                        atom_1 = int(fields[0])
+                        if atom_1 in bonds[:,1]:
+                            continue
+                        else:
+                            atom_2 = int(fields[3+b])
+                            bond_order = float(fields[4+nb+b])
+                            bonds = np.vstack([bonds, [atom_1, atom_2, bond_order]])
+    return bonds
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mtools',
+    version='0.1',
+    description='Matt has all of the MD tools in the world',
+    url='http://github.com/mattwthompson/mtools',
+    author='Matt Thompson',
+    author_email='mattwthomp@gmail.com',
+    license='None',
+    packages=['mtools'],
+    zip_safe=False)
