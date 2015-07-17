71070625a565a60afff078dbeb1dd0d6ca646e6e
mattwthompson <mattwthomp@gmail.com>
2015-07-16 19:11:10 -0700

    added coordination number calculator

--- mtools/reaxff/reaxff.py
+++ mtools/reaxff/reaxff.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def read_reaxc_bonds(bonds_file = 'bonds.reaxc', cutoff=0.3):
-    """Takes bonds file, minimin 'bond order' to consider
+    """Takes bonds file, minimum 'bond order' to consider
 
     Returns
     -------
@@ -27,3 +27,17 @@ def read_reaxc_bonds(bonds_file = 'bonds.reaxc', cutoff=0.3):
                             bond_order = float(fields[4+nb+b])
                             bonds = np.vstack([bonds, [atom_1, atom_2, bond_order]])
     return bonds
+
+
+def calculate_coordination_number(bonds, cutoff=0.3):
+    """Takes array of bond indices, minimum 'bond order' to consider
+    
+    Returns
+    -------
+    coordination_number : np.array, shape=(n, 1), dtype=int
+    """
+    coordination_number = list()
+    for atom in np.unique(bonds[:,:2]):
+        coordination_number.append(len([row for row in bonds[np.where(bonds[:,[0,1]] == atom),:][0] if row[2] > cutoff]))
+    coordination_number = np.array(coordination_number)
+    return coordination_number
