654a2308246cab092d809a5d1d3ec8d6fba40b8d
mattwthompson <mattwthomp@gmail.com>
2017-01-27 09:23:50 -0600

    Update syntax

--- examples/test_num.py
+++ examples/test_num.py
@@ -1,6 +1,6 @@
 from mtools.post_process import calc_number_density
 
-calc_number_density.calc_number_density(gro_file='water-box/confout.pdb',
+calc_number_density.calc_number_density(coord_file='water-box/confout.pdb',
     trj_file='water-box/traj.xtc', ions={'SOL': ['OW', 'HW1', 'HW2']},
     bin_width=0.01, area=3.0129*3.0129, dim=0, box_range=[0, 3.0129],
     data_path='data')
--- mtools/post_process/calc_number_density.py
+++ mtools/post_process/calc_number_density.py
@@ -1,31 +1,13 @@
 import numpy as np
 import mdtraj as md
-import pdb
-import matplotlib.pyplot as plt
 
-#gro_file = 'test-data/confout.gro'
-#trj_file = 'test-data/traj.xtc'
-#
-#bin_width = 0.067/2
-#area = 6.15*5.538
-#
-#first_frame = md.load_frame(trj_file, top=gro_file, index=0)
-#
-#ions = dict()
-#ions['EMIM'] = first_frame.topology.select('resname EMIM')
-#ions['Tf2N'] = first_frame.topology.select('resname Tf2N')
-#atoms_per_ion = dict()
-#atoms_per_ion['EMIM'] = 19
-#atoms_per_ion['Tf2N'] = 15
-
-def calc_number_density(gro_file, trj_file, ions,
+def calc_number_density(coord_file, trj_file, ions,
     bin_width, area, dim, box_range, data_path):
     
     for ion in np.sort(list(ions.keys())):
-        #first_frame = md.load_frame(trj_file, top=gro_file,
+        #first_frame = md.load_frame(trj_file, top=coord_file,
         #    index=0, atom_indices=ions[ion])
-        traj = md.load(trj_file, top=gro_file)#, atom_indices=ions[ion])
-        print(traj)
+        traj = md.load(trj_file, top=coord_file)#, atom_indices=ions[ion])
         indices = [[at.index for at in compound.atoms] for compound in list(
             traj.topology.residues)]
         com = list()
@@ -33,7 +15,6 @@ def calc_number_density(gro_file, trj_file, ions,
             sub_traj = traj.atom_slice(ids)
             com.append(md.compute_center_of_mass(sub_traj))
         com = np.array(com)
-        print(np.shape(com))
         x = np.histogram(com[:,1:,dim].reshape((-1, 1)),
             bins=np.linspace(box_range[0], box_range[1],
             num=1+round((box_range[1] - box_range[0])/bin_width)))
