49a489b5cc5b80da3c5b9bd3d0deca273e85c8b7
mattwthompson <mattwthomp@gmail.com>
2017-03-20 17:57:37 -0500

    Add custom 9-6 writer

--- mtools/gromacs/gromacs.py
+++ mtools/gromacs/gromacs.py
@@ -31,3 +31,22 @@ def read_itp(itp_file):
                 charges[atom_data[4]] = atom_data[6]
                 masses[atom_data[4]] = atom_data[7]
     return charges, masses
+
+
+def write_custom_96(file_name):
+    """Writes .xvg file for tabulated 9-6 potential
+
+    See http://www.gromacs.org/@api/deki/files/94/=gromacs_nb.pdf
+
+    Arguments
+    -------
+    file_name : string
+    """
+    dr = 0.002
+    rcut = 1.0
+    nbins = int((rcut + 1)/dr + 1)
+    with open(file_name, 'w') as fo:
+        for j in range(1, nbins):
+            r = dr*j
+            fo.write('{0} {1} {2} {3} {4} {5} {6}'.format(
+                r, 1/r, 1/(r*r), -1/(r**6), -6/(r**7), 1/(r**9), 9/(r**10)))
