ea351a1a8634eb75bd6303c5b065b41c14e065c7
mattwthompson <mattwthomp@gmail.com>
2017-03-20 21:31:49 -0500

    Cleanup

--- mtools/gromacs/gromacs.py
+++ mtools/gromacs/gromacs.py
@@ -42,11 +42,34 @@ def write_custom_96(file_name):
     -------
     file_name : string
     """
-    dr = 0.002
-    rcut = 1.0
+    dr = 0.001
+    rcut = 2.0
     nbins = int((rcut + 1)/dr + 1)
     with open(file_name, 'w') as fo:
         for j in range(1, nbins):
             r = dr*j
-            fo.write('{0} {1} {2} {3} {4} {5} {6}'.format(
+            fo.write('{0} {1} {2} {3} {4} {5} {6}\n'.format(
                 r, 1/r, 1/(r*r), -1/(r**6), -6/(r**7), 1/(r**9), 9/(r**10)))
+
+
+def write_custom_lj_mn(file_name='table.xvg', m=12, n=6, dr=0.001, rcut=1.5):
+    """Writes .xvg file for generic Lennard Jones-like n-m potential 
+
+    See http://www.gromacs.org/@api/deki/files/94/=gromacs_nb.pdf
+
+    Arguments
+    -------
+    file_name : string, path of table file
+    m : int, order of repulsive term
+    n : int, order of attractive term
+    dr : float, spacing of values, nm
+    rcut : float, cutoff distance, nm
+    """
+    dr = 0.001
+    rcut = 2.0
+    nbins = int((rcut + 1)/dr + 1)
+    with open(file_name, 'w') as fo:
+        for j in range(1, nbins):
+            r = dr*j
+            fo.write('{0} {1} {2} {3} {4} {5} {6}\n'.format(
+                r, 1/r, 1/(r*r), -1/(r**n), -n/(r**(n+1)), 1/(r**m), m/(r**(m+1))))
