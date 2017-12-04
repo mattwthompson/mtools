ee70aed12b9cb68649ef406af2ed9ad667b09930
mattwthompson <matt.thompson@vanderbilt.edu>
2017-12-04 11:25:10 -0600

    Cleanup and pep8 fixes

--- mtools/pairing.py
+++ mtools/pairing.py
@@ -9,7 +9,7 @@ def calc_pairing(trj, cutoff, names, chunk_size=100, normalize=False):
     for i, frame in enumerate(trj):
         if i % chunk_size == 0:
             pairs = build_initial_state(frame, frame_index=0,
-                names=names, cutoff=cutoff)
+                                        names=names, cutoff=cutoff)
         # If no pairs were found, set number of pairs to 0
         if len(pair) == 0:
             c[i] = 0
@@ -17,12 +17,12 @@ def calc_pairing(trj, cutoff, names, chunk_size=100, normalize=False):
         for pair in pairs:
             if pair[2]:
                 pair[2] = get_paired_state(frame, pair[0], pair[1],
-                    frame_index=0, cutoff=0.8)
+                                           frame_index=0, cutoff=0.8)
         c[i] = [frame.time[0], np.sum(np.array(pairs)[:, 2])]
 
     hbar = np.zeros(shape=(chunk_size, 2))
 
-   for chunk in chunks(c, chunk_size):
+    for chunk in chunks(c, chunk_size):
         if len(chunk) < chunk_size:
             continue
         hbar[:, 0] = chunk[:, 0] - chunk[0, 0]
@@ -33,18 +33,21 @@ def calc_pairing(trj, cutoff, names, chunk_size=100, normalize=False):
 
     return hbar
 
-    
+
 def get_paired_state(trj, id_i, id_j, frame_index=0, cutoff=1):
     """Check to see if a given pair is still paired."""
-    dist = np.sum(np.sqrt((trj.xyz[frame_index, id_i] - trj.xyz[frame_index, id_j])**2))
+    dist = np.sum(np.sqrt((trj.xyz[frame_index, id_i] -
+                           trj.xyz[frame_index, id_j]) ** 2))
     if dist < cutoff:
         paired = True
     else:
         paired = False
     return paired
 
+
 def build_initial_state(trj, names, frame_index=0, cutoff=1):
-    """Build initial pair list. See 10.1021/acs.jpclett.5b00003 for a definition. The re-forming of pairs is supported implicitly."""
+    """Build initial pair list. See 10.1021/acs.jpclett.5b00003 for a
+    definition. The re-forming of pairs is supported implicitly."""
     atom_ids = [a.index for a in trj.topology.atoms if a.name in names]
     pairs = [prod for prod in itertools.combinations(atom_ids, r=2)]
     pairs = [list([*pair, False]) for pair in pairs]
@@ -52,23 +55,26 @@ def build_initial_state(trj, names, frame_index=0, cutoff=1):
     for i, pair in enumerate(pairs):
         if pair[0] == pair[1]:
             continue
-        pair[2] = get_paired_state(trj, pair[0], pair[1], frame_index=frame_index)
+        pair[2] = get_paired_state(trj, pair[0], pair[1],
+                                   frame_index=frame_index)
     pairs = [pair for pair in pairs if pair[2] == True]
 
     return pairs
 
-def chunks(l, n): 
+
+def chunks(l, n):
     """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n): 
+    for i in range(0, len(l), n):
         yield l[i:i + n]
 
-def stretched_exp(x, a, b): 
+
+def stretched_exp(x, a, b):
     """Define a stretched exponential function."""
     f = np.exp(-1 * b * x ** a)
     return f
 
+
 def fit(func, t, n_pairs):
-    """Fit pairing data to 
+    """Fit pairing data to a stretched exponential function"""
     popt, pcov = curve_fit(stretched_exp, t, n_pairs)
     return popt
-
